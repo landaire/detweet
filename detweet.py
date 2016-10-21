84c0a11c17a2c0d688f5ce389b8f4565b527738e
Lander Brandt <landerbrandt@gmail.com>
2016-10-20 19:28:24 -0700

    Add before and after dates

--- detweet.py
+++ detweet.py
@@ -7,17 +7,23 @@ import os
 import sys
 import csv
 import re
+import moment
 
 ENVIRON_CONSUMER_KEY = 'CONSUMER_KEY'
 ENVIRON_CONSUMER_SECRET = 'CONSUMER_SECRET'
 ENVIRON_ACCESS_TOKEN = 'ACCESS_TOKEN'
 ENVIRON_ACCESS_TOKEN_SECRET = 'ACCESS_TOKEN_SECRET'
 
+DATE_FORMAT = 'YYYY-M-D'
+TWEET_TIMESTAMP_FORMAT = 'YYYY-MM-DD'
+
 
 def main():
     parser = argparse.ArgumentParser(description='Delete tweets en masse')
     parser.add_argument('--csv', help='the path to your tweets.csv file located in your twitter archive', required=True)
     parser.add_argument('--dry', type=bool, default=False, help='do a dry run')
+    parser.add_argument('--before', help='match tweets before this date (YYYY-M-D)')
+    parser.add_argument('--after', help='match tweets after this date (YYYY-M-D)')
     parser.add_argument('pattern', help='regular expressions to match', nargs='+')
 
     args = parser.parse_args()
@@ -42,11 +48,28 @@ def main():
 
         exit(1)
 
+    before_date = None
+    after_date = None
+
+    if args.before is not None:
+        before_date = moment.date(args.before, DATE_FORMAT)
+    if args.after is not None:
+        after_date = moment.date(args.after, DATE_FORMAT)
+
     with open(args.csv) as csv_file:
         reader = csv.DictReader(csv_file)
 
         matching_tweets = []
         for row in reader:
+            # slice the date here so that we don't bother parsing the time
+            tweet_date = moment.date(row['timestamp'][:len(TWEET_TIMESTAMP_FORMAT)], TWEET_TIMESTAMP_FORMAT)
+
+            if before_date is not None and tweet_date > before_date:
+                continue
+
+            if after_date is not None and tweet_date < after_date:
+                continue
+
             if tweet_matches_patterns(row['text'], args.pattern):
                 matching_tweets.append(row)
 
